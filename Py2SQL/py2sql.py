9ec39c9065a61dabe898583722103179150b402f
Nirfiolo <bodyaufml@gmail.com>
2020-12-12 22:23:53 +0200

    Add db_disconnect, db_engine, db_name, db_size, db_tables, db_table_structure, db_table_size function

--- Py2SQL/py2sql.py
+++ Py2SQL/py2sql.py
@@ -22,31 +22,61 @@ class Py2SQL(metaclass=InitLocker):
 
     @staticmethod
     def db_disconnect():
-        pass
+        Py2SQL.__check_connection()
+        Py2SQL.__database_connection.disconnect()
+        Py2SQL.__database_connection = None
 
     @staticmethod
     def db_engine():
-        pass
+        Py2SQL.__check_connection()
+
+        name = Py2SQL.__select_single_query('SELECT DATABASE()')
+        version = Py2SQL.__select_single_query('SELECT VERSION()')
+
+        return 'Name: {0}, Version: {1}'.format(name, version)
 
     @staticmethod
     def db_name():
-        pass
+        Py2SQL.__check_connection()
+
+        return Py2SQL.__select_single_query('SELECT DATABASE()')
 
     @staticmethod
     def db_size():
-        pass
+        Py2SQL.__check_connection()
+
+        return Py2SQL.__select_single_query('SELECT ROUND(SUM(data_length + index_length) / 1024 / 1024, 3) '
+                                            'FROM information_schema.tables '
+                                            'WHERE table_schema = \'{0}\' '
+                                            'GROUP BY table_schema;'.format(Py2SQL.db_name()))
 
     @staticmethod
     def db_tables():
-        pass
+        Py2SQL.__check_connection()
+
+        cursor = Py2SQL.__database_connection.cursor()
+        cursor.execute('SHOW TABLES')
+        data = cursor.fetchall()
+        return [x[0] for x in data]
 
     @staticmethod
     def db_table_structure(table):
-        pass
+        Py2SQL.__check_connection()
+
+        cursor = Py2SQL.__database_connection.cursor()
+        cursor.execute('DESCRIBE {0};'.format(table))
+        data = cursor.fetchall()
+        return [(i, x[0], x[1]) for i, x in enumerate(data)]
 
     @staticmethod
     def db_table_size(table):
-        pass
+        Py2SQL.__check_connection()
+
+        return Py2SQL.__select_single_query('SELECT ROUND((DATA_LENGTH + INDEX_LENGTH) / 1024 / 1024, 3)'
+                                            'FROM information_schema.TABLES '
+                                            'WHERE TABLE_SCHEMA = "{0}" '
+                                            'AND TABLE_NAME = "{1}" '
+                                            'ORDER BY (DATA_LENGTH + INDEX_LENGTH);'.format(Py2SQL.db_name(), table))
 
     @staticmethod
     def find_object(table, py_object):
@@ -79,3 +109,15 @@ class Py2SQL(metaclass=InitLocker):
     @staticmethod
     def create_hierarchy(table, package):
         pass
+
+    @staticmethod
+    def __check_connection():
+        if Py2SQL.__database_connection is None:
+            raise ValueError('Database isn\'t connected')
+
+    @staticmethod
+    def __select_single_query(query):
+        cursor = Py2SQL.__database_connection.cursor()
+        cursor.execute(query)
+        data = cursor.fetchone()
+        return data[0]
