a4c11e267b3825ececaefeac69a68c8ee7e32771
Nirfiolo <bodyaufml@gmail.com>
2020-12-13 21:02:57 +0200

    Add setup and readme file

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+]
+
+setup(
+    name="symphony_of_nirfolio_py2sql",
+    version="0.0.6",
+    author="",
+    author_email="",
+    description="",
+    long_description="",
+    install_requires=["mysql.connector"],
+    url="https://htmlpreview.github.io/?https://github.com/symphony-of-nirfolio-uni/Metaprogramming-lab3/blob/dev/documentation/Py2SQL.html",
+    packages=find_packages(),
+    python_requires='>=3.6',
+)
