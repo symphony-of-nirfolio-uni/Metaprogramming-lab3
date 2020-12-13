62f62abee727e43d7b6b1a31badeb9cda595cb8b
SymphonyOfTranquility <mysechko01@gmail.com>
2020-12-13 14:55:17 +0200

    Add documentation to __init__ file, create_hierarchy, create_class and create_object

---
+++ Py2SQL/__init__.py
@@ -0,0 +1,7 @@
+"""
+Package that implements partial translation of instance objects,
+class objects and class hierarchies in Python from MySQL
+"""
+
+from .database_info import DatabaseInfo
+from .py2sql import Py2SQL
